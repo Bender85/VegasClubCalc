97ae712ffe676faa80fcc0e26dd287fc8dd2afbb
nikita.goloshchapov <nikita.goloshchapov@codeit.com.ua>
2018-02-01 18:10:21 +0200

    add files

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+// You can write a call and import your functions in this file.
+//
+// This file will be compiled into app.js and will not be minified.
+// Feel free with using ES6 here.
+
+import DE from './modules/helpers';
+import customSelect from './modules/customSelect';
+import calculator from './modules/calculator';
+
+( ($) => {
+  'use strict';
+
+  // When DOM is ready
+  $(() => {
+    // DE.dotsEffect();
+    customSelect.selectFunc();
+    // calculator.calc();
+  });
+
+})(jQuery);
